2e70808937d46f29bcd58972cb5467867f92c53f
Csaba Ovari <csabikahhh@gmail.com>
2023-09-24 15:55:32 +0200

    some update for a tags

--- pages/index.js
+++ pages/index.js
@@ -45,13 +45,13 @@ export default function Home() { const [darkMode, setDarkMode] = useState(true);
                 <h2 className="text-5xl py-2 text-fuchsia-600 font-medium dark:text-fuchsia-400 md:text-6xl"> Horváth Dénes </h2>
                 <h3 className="text-2xl py-2 dark:text-white md:text-3xl"> Junior szoftverfejlesztő </h3>
                 <div className="text-5xl flex justify-center gap-16 pt-12 py-3 text-gray-600 dark:text-gray-400">
-                    <a href="https://github.com/HDenes8" target="_blank" rel="noreferrer">
+                    <a href="https://github.com/HDenes8" target="_blank" rel="noreferrer" aria-label="GitHub link">
                         <AiFillGithub />
                     </a>
-                    <a href="https://www.linkedin.com/in/denes-horvath/" target="_blank" rel="noreferrer">
+                    <a href="https://www.linkedin.com/in/denes-horvath/" target="_blank" rel="noreferrer" aria-label="LinkedIn link">
                         <AiFillLinkedin />
                     </a>
-                    <a href="mailto:denes.horvath.email@gmail.com?subject=Üzenet">
+                    <a href="mailto:denes.horvath.email@gmail.com" aria-label="Mail link">
                         <AiOutlineMail />
                     </a>
                     <a href="tel:+36306504405" target="_blank" rel="noreferrer">
